6435aeeb25b1ab971effae50d38a72210ebd0a7e
5i03 <62823952+5i03@users.noreply.github.com>
2023-10-19 14:17:16 +0800

    Update qiangke.py
    使用了多线程，提升抢课速度

--- qiangke.py
+++ qiangke.py
@@ -1,46 +1,41 @@
-import time  # 时间模块
-import requests  # 网络请求模块
-import os  # 系统模块
-import re  # 正则表达式模块
-# from crypto.Cipher import DES  # 加密模块
-import base64  # base64模块
-import logging  # 日志模块
+import requests
+import os
 import subprocess
 import platform
-class enrollment:
-    
-    def __init__(self, ):  # 初始化
-        # logging.basicConfig() # 日志配置
-        self.session = requests.Session()  # 创建会话
-        self.session.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}  # 设置请求头
+import threading
 
-    # 验证码处理
-    def verfiyCode(self):
-        capURL='http://10.1.1.100/studentportal.php/Public/verify/'
-        r = self.session.get(capURL, verify=False)
+
+class Enrollment:
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/87.0.4280.88 Safari/537.36'
+        }
+
+    def verify_code(self):
+        cap_url = 'http://10.1.1.100/studentportal.php/Public/verify/'
+        r = self.session.get(cap_url, verify=False)
         capimg = r.content
         with open('captcha.png', 'wb') as f:
             f.write(capimg)
         print("验证码已保存,请打开软件所在目录查看“captcha.png”并输入")
-        # 打开captcha.png
-        # os.open()
         filepath = os.path.join(os.getcwd(), 'captcha.png')
-        if platform.system() == 'Darwin':  # macOS
+        if platform.system() == 'Darwin':
             subprocess.call(('open', filepath))
-        elif platform.system() == 'Windows':  # Windows
+        elif platform.system() == 'Windows':
             os.startfile(filepath)
-        else:  # linux variants
+        else:
             subprocess.call(('xdg-open', filepath))
         self.code = input("在此输入：")
         os.remove('captcha.png')
         return 1
 
-    # 登录
-    def login(self, stuid, stupwd):
-        loginURL='http://10.1.1.100/studentportal.php/Index/checkLogin'
-        res = self.session.post(loginURL, data={
-                                "logintype": "xsxh", 'xsxh': stuid, 'dlmm': stupwd, 'yzm': self.code}, verify=False).json()
+    def login(self, stu_id, stupwd):
+        login_url = 'http://10.1.1.100/studentportal.php/Index/checkLogin'
+        res = self.session.post(login_url, data={
+            "logintype": "xsxh", 'xsxh': stu_id, 'dlmm': stupwd, 'yzm': self.code}, verify=False).json()
         if res['code'] == 3:
             print('验证码错误！')
             return 0
@@ -48,67 +43,57 @@ class enrollment:
             return 1
         else:
             print(res)
-            print('登录异常, 出现未知错误！')
-            time.sleep(3)
+            print('登录异常, 未知错误！')
             exit()
 
-    # 获取选修课列表
-    def courseInfo(self):
-        courseURL='http://10.1.1.100/studentportal.php/Wsxk/yjxklb/'
+    def course_info(self):
+        course_url = 'http://10.1.1.100/studentportal.php/Wsxk/yjxklb/'
         courselist = []
-        res = self.session.post(courseURL, data={
-                                "page": "1", "rows": "50"}, verify=False).json()['rows']
-        for i, list in enumerate(res):
-
-            id = list['id']  # 抢课软件显示序列号
-            courselist.append(id)
-            name = list['kcmc']   # 课程名称
-            tech = list['zdjsxm']  # 任课老师姓名
-            # leixin = list["kcflmc"]
-            rang = list["kkxqmc"]  # 上课区域
-            xkid = list["xkxxid"]  # 提交的选修课ID
-            xqzxs = list["xqzxs"]  # 可以获得的学时
-            xkrsrl = list["xkrsrl"]# 限制选定人数
-            xkyxrs= list["xkyxrs"]# 已选定人数
-            # print(f"{i + 1}.{name} {tech} {leixin} {rang}")
-            print(f"{i + 1}.  |课名： {name}  |老师： {tech} |课号： {xkid} |学时： {xqzxs} | 余位："+ str(int(xkrsrl)-int(xkyxrs)))
+        res = self.session.post(course_url, data={"page": "1", "rows": "50"}, verify=False).json()['rows']
+        for i, data in enumerate(res):
+            courselist.append(data['id'])
+            print(
+                f"{i + 1}. | 课名： {data['kcmc']} | 老师： {data['zdjsxm']} | 课号： {data['id']} | 学时： {data['xqzxs']} | 余位: {int(data['xkrsrl']) - int(data['xkyxrs'])}")
         return courselist
 
-    # 发送请求数据
-    def processingData(self, ID):
-        enrollURL='http://10.1.1.100/studentportal.php/Wsxk/yjxkbc'
-        reponse = self.session.post(enrollURL, data={'xkxxid': ID, 'xktjid': 0}).json()
-        if reponse["Code"] == 0:
-            print(reponse["errorMsg"])
-        elif reponse["Code"] == 1:
-            print('恭喜你成功抢到课了！')
+    def enroll_thread(self, course_id):
+        enroll_url = 'http://10.1.1.100/studentportal.php/Wsxk/yjxkbc'
+        response = self.session.post(enroll_url, data={'xkxxid': course_id, 'xktjid': 0}).json()
+        if response["Code"] == '0':
+            print('抢课',course_id,'失败,是因为',response["errorMsg"])
+        elif response["Code"] == '1':
+            print('抢课',course_id,"成功,还可选",str(4-int(response["rxkxsyxs"])),"门")
         else:
-            time.sleep(1)
-            print(reponse)
-
+            print(response)
+            # print(response["Code"])
+            pass
 
 if __name__ == "__main__":
-
-    c = enrollment()
+    c = Enrollment()
     print('开始登录')
     while True:
-        # 登录
-        stuID = ""  # 教务系统学号
-        stuPass = ""  # 教务系统密码
-
-        if c.verfiyCode() and c.login(stuID, stuPass):
-            print('登录成功！学号：'+stuID+'密码：'+stuPass + '，请稍后\n'+'即将进入选课系统！')
-            xkid = c.courseInfo()
+        # 教务系统学号密码
+        stu_id = ""
+        stu_pwd = ""
+        if c.verify_code() and c.login(stu_id, stu_pwd):
+            print('登录成功！学号：' + stu_id + '，请稍后\n' + '即将进入选课系统！')
+            xkid = c.course_info()
+            threads = []
             while True:
-                courseid = int(input("请输入课程序号(0退出）："))  # 输入课程序号
-                if courseid > len(xkid):
-                    print("没有对应课程！再试一次？")
-                    continue
-                courseId = xkid[courseid - 1]
-                c.processingData(courseId)
+                courseid = int(input("请输入课程序号(0退出）："))
+
                 if courseid == 0:
                     break
+                if courseid <= len(xkid):
+                    courseId = xkid[courseid - 1]
+                    thread = threading.Thread(target=c.enroll_thread, args=(courseId,))
+                    thread.start()
+                    threads.append(thread)
+                else:
+                    print("没有对应课程！再试一次？")
+
+            for thread in threads:
+                thread.join()
             break
         else:
-            print('即将将重新登录, 请等待...')
-            time.sleep(3)
+            print('即将重新登录, 请等待...')
